2f65ff89dd65529e053f236e1e802ea279dd7de7
nikhilmulik2002 <nikhilmulik2002@gmail.com>
2017-11-08 19:50:51 -0500

    watch functions changes

--- watch_for_changes.py
+++ watch_for_changes.py
@@ -15,10 +15,17 @@ class MyHandler(PatternMatchingEventHandler):
     def process(self, event):
         # print event.src_path, event.event_type
         if event.event_type == 'modified':
+            # print 'File CREATED: ',event.src_path
             url = 'http://localhost:5000/update_webcam_capture/'
             files = {'media': open(event.src_path, 'rb')}
             requests.post(url, files=files)
 
+
+    def on_modified(self, event):
+        self.process(event)
+
+    def on_created(self, event):
+        self.process(event)
 
 if __name__ == '__main__':
     args = sys.argv[1:]
